9b7efe870b7353d6ac5c5354a6162b723afcb5b4
Neophyliam <727549953@qq.com>
2019-05-30 23:13:53 +0800

    exer2_1

---
+++ chap2/exer2_1.scm
@@ -0,0 +1,36 @@
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+; (define (make-rat n d) (cons n d))
+(define (make-rat n d)
+  (define nn (if (< d 0) (- n) n))
+  (define nd (if (< d 0) (- d) d))
+  (let ((g (gcd nn nd)))
+    (cons (/ nn g) (/ nd g))))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
