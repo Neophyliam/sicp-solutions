11cf2091c0110f813da10ea8e1b044a8ca4be291
Neophyliam <727549953@qq.com>
2019-09-15 14:53:30 +0800

    exer3_21..exer3_23

---
+++ chap3/exer3_22.scm
@@ -0,0 +1,71 @@
+(define (println x)
+  (display x)
+  (newline))
+
+(define (make-queue)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (empty-queue?) (null? front-ptr))
+
+    (define (front-queue)
+      (if (empty-queue?)
+          (error "FRONT called with an empty queue")
+          (car front-ptr)))
+
+    (define (insert-queue! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-queue?)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair))
+              (else
+                (set-cdr! rear-ptr new-pair)
+                (set! rear-ptr new-pair)))))
+
+    (define (delete-queue!)
+      (cond ((empty-queue?)
+             (error "DELETE! called with an empty queue"))
+            (else
+              (set! front-ptr (cdr front-ptr)))))
+
+    (define (print-queue)
+      (define (iter item)
+        (cond ((null? item) (println ")"))
+              (else
+                (display (car item))
+                (display " ")
+                (iter (cdr item)))))
+      (display "(")
+      (iter front-ptr))
+
+    (define (dispatch m)
+      (cond ((eq? m 'empty-queue?) (empty-queue?))
+            ((eq? m 'front-queue) (front-queue))
+            ((eq? m 'insert-queue!) insert-queue!)
+            ((eq? m 'delete-queue!) (delete-queue!))
+            ((eq? m 'print-queue) (print-queue))
+            (else
+              (error "Undefined operation -- " m))))
+    dispatch))
+
+(define (empty-queue? queue) (queue 'empty-queue?))
+(define (front-queue queue) (queue 'front-queue))
+(define (insert-queue! queue item)
+  ((queue 'insert-queue!) item)
+  queue)
+(define (delete-queue! queue)
+  (queue 'delete-queue!)
+  queue)
+(define (print-queue queue)
+  (queue 'print-queue))
+
+
+;; test
+(define q1 (make-queue))
+(println (insert-queue! q1 'a))
+(print-queue q1)
+(println (insert-queue! q1 'b))
+(print-queue q1)
+(println (delete-queue! q1))
+(print-queue q1)
+(println (delete-queue! q1))
+(print-queue q1)
