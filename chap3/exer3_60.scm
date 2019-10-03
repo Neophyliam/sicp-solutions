d61843b7c1ee67b2a4913c1a2dd82838f85c4306
neothenil <727549953@qq.com>
2019-10-03 13:18:07 +0800

    update exer3_60.scm

--- chap3/exer3_60.scm
+++ chap3/exer3_60.scm
@@ -3,4 +3,5 @@
                (add-stream
                  (scale-stream (stream-cdr s2) (stream-car s1))
                  (scale-stream (stream-cdr s1) (stream-car s2))
-                 (mul-series (stream-cdr s1) (stream-cdr s2)))))
+                 (cons-stream 0
+                              (mul-series (stream-cdr s1) (stream-cdr s2))))))
